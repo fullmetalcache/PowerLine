27cffb56d3fecad3ec05724f7dec72a978b6fd9e
fmc <fmc@WIN-TMKUISIEHKB>
2017-03-29 08:43:21 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
