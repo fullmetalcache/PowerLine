27196a13e737d8a3da292a1ea97f78a3629afbdd
fmc <fullmetalcache@gmail.com>
2017-06-07 19:19:20 -0600

    Updated build.bat to correctly target config/platform for x64

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe %~dp0\PowerLine.sln /t:rebuild /p:PlatformTarget=x64
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe %~dp0\PowerLine.sln /t:rebuild /p:Configuration=Release /p:Platform=x64
 
 ECHO ON
