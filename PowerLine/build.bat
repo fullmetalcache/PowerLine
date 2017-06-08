f28ab79d2c5853b5d6b4e12e8f35cff0b71b0c52
fullmetalcache <fullmetalcache@gmail.com>
2017-06-08 12:04:14 -0600

    Removed debugging changes from bat file

--- PowerLine/build.bat
+++ PowerLine/build.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
-if exist C:\Windows\Microsoft.NET\Framework6sdf4\v2.0.50727\MSBuild.exe (
-	C:\Windows\Microsoft.NET\Framework6gf4\v2.0.50727\MSBuild.exe %~dp0\PowerLineWin7.pln.sln /t:rebuild /p:Configuration=Release /p:Platform=x64
+if exist C:\Windows\Microsoft.NET\Framework64\v2.0.50727\MSBuild.exe (
+	C:\Windows\Microsoft.NET\Framework64\v2.0.50727\MSBuild.exe %~dp0\PowerLineWin7.pln.sln /t:rebuild /p:Configuration=Release /p:Platform=x64
 ) else if exist C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe (
 	C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe %~dp0\PowerLineWin10.pln.sln /t:rebuild /p:Configuration=Release /p:Platform=x64
 ) else (
