1144d82eeae225043dacc0152f922c23d0976e01
fmc <fullmetalcache@gmail.com>
2017-06-07 23:28:26 -0600

    Added Fody Package

--- PowerLine/build.bat
+++ PowerLine/build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-copy C:\Windows\
+copy "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Microsoft.Build.Tasks.v4.0.dll" "C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\Microsoft.Build.Tasks.v4.0.dll"
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe %~dp0\PowerLine.sln /t:rebuild /p:Configuration=Release /p:Platform=x64
 
 ECHO ON
